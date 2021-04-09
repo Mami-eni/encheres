0bdf5b844f3f5be1109ec7c39604bbea873e8fc9
mamibetina <none>
2021-04-09 13:29:05 +0200

    Merge avec depot principal et mise à jour du lien local vers les images

--- src/fr/eni/ecole/view/ViewConstants.java
+++ src/fr/eni/ecole/view/ViewConstants.java
@@ -2,6 +2,6 @@ package fr.eni.ecole.view;
 
 public interface ViewConstants {
 
-	public static final String IMAGE_PATH = "C:/Users/fraud et med/git/encheres/WebContent/imagesArticles";
+	public static final String IMAGE_PATH = "C:/Users/mamib/Documents/cours_ENI/Modules/projet-troc-version-commune/encheres/WebContent/imagesArticles";
 	
 }
